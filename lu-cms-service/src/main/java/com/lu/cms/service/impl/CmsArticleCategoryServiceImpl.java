1879b48473f81e8287291e1281d18018b83d6d95
Anxiaolu <1466022881@qq.com>
2017-11-03 21:51:01 +0800

    添加简单Service实现

--- lu-cms-service/src/main/java/com/lu/cms/service/impl/CmsArticleCategoryServiceImpl.java
+++ lu-cms-service/src/main/java/com/lu/cms/service/impl/CmsArticleCategoryServiceImpl.java
@@ -5,10 +5,45 @@
  */
 package com.lu.cms.service.impl;
 
+import com.lu.cms.model.CmsArticleCategory;
+import com.lu.cms.service.CmsArticleCategoryService;
+
 /**
  *
  * @author huanlu
  */
-public class CmsArticleCategoryServiceImpl {
+public class CmsArticleCategoryServiceImpl implements CmsArticleCategoryService{
+
+    
+    
+    @Override
+    public int deleteByPrimaryKey(Integer articleCategoryId) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int insert(CmsArticleCategory record) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int insertSelective(CmsArticleCategory record) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public CmsArticleCategory selectByPrimaryKey(Integer articleCategoryId) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int updateByPrimaryKeySelective(CmsArticleCategory record) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int updateByPrimaryKey(CmsArticleCategory record) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
